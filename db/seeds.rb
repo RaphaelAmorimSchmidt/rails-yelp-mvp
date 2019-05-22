e1e575f8c99a965d308ca9cd3066cf6d1369505f
RaphaelAmorim Schmidt <raphaschmidt89@gmail.com>
2019-05-22 14:57:31 -0300

    controller and routes ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating new records"
+
+Restaurant.destroy_all
+puts "Creating restaurants..."
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number: '01 02 03 04'
+
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '05 06 07 08'
+
+  },
+  {
+    name:         'Vintão',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'french',
+    phone_number: '01 02 03 04'
+
+  },
+  {
+    name:         'sushiiisim',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'japanese',
+    phone_number: '05 06 07 08'
+
+  },
+  {
+    name:         'alguma coisa',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'belgian',
+    phone_number: '01 02 03 04'
+
+  },
+  {
+    name:         'Pizza West',
+    address:      'outro lugar',
+    category:     'italian',
+    phone_number: '05 06 07 08'
+
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts "#{Restaurant.count} restaurants created"
+puts "Creating reviews..."
+puts "#{Review.count} restaurants created"
+puts "Finished!"
+
